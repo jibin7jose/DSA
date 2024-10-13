730770d38aac5b774a70f76fcc688bae83a446aa
jibin jose <jibinjose884@gmail.com>
2024-10-14 00:16:53 +0530

    Update N-by-2-Forest.java

--- N-by-2-Forest.java
+++ N-by-2-Forest.java
@@ -1,1 +1,23 @@
 
+Input:  ‘N’ = 3
+
+Output: 
+* 
+* *
+* * *
+
+
+
+code
+
+  public class Solution {
+    public static void nForest(int n) {
+     
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* "); // Print star followed by a space
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
