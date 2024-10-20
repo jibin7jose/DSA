4cabd5dbb121c2f6bc721ba7d8ca138b018d9ba5
jibin jose <jibinjose884@gmail.com>
2024-10-20 22:45:36 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -12,7 +12,7 @@ code
 
 public class Solution {
     public static void nTriangle(int n) {
-        // Write your code here
+
         for(int i=0;i<n;i++){
             for(int j=0;j<i+1;j++){
                 System.out.printf("%d ",i+1);
