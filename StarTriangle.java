94f9ce4be8276cba757c01302d496a3ba4c262c9
jibin jose <jibinjose884@gmail.com>
2024-10-27 17:07:06 +0530

    Create StarTriangle.java

---
+++ StarTriangle.java
@@ -0,0 +1,29 @@
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+
+
+code
+
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i=0;i<n;i++){
+            int space=n-i-1;
+            for(int j=0;j<space;j++){
+                System.out.printf(" ");
+            }
+            for(int j=0;j<(2*i+1);j++){
+                System.out.printf("*");
+            }
+             for(int j=0;j<space;j++){
+                System.out.printf(" ");
+        }
+        System.out.println();
+        }
+    }
+}
