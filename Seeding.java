372f09d1ac0d09c49c9b1d0b78ee3c7345447078
jibin jose <jibinjose884@gmail.com>
2024-10-27 17:08:25 +0530

    Update Seeding.java

--- Seeding.java
+++ Seeding.java
@@ -10,7 +10,6 @@ Output:
 
 public class Solution {
     public static void seeding(int n) {
-        // Write your code here
         int i,j;
            for ( i = 0; i <= n; i++) {
             for ( j = n; j > i; j--) {
