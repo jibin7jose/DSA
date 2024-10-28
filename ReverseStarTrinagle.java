e94684a0f746a2999a17278390880843e19a9659
jibin jose <jibinjose884@gmail.com>
2024-10-28 18:54:16 +0530

    Create ReverseStarTrinagle.java

---
+++ ReverseStarTrinagle.java
@@ -0,0 +1,30 @@
+Input: ‘N’ = 3
+
+Output: 
+
+*****
+ ***
+  *
+
+
+code
+
+
+  public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i=n-1;i>=0;i--){
+             int space=n-i-1;
+           for(int j=0;j<space;j++){
+                System.out.printf(" ");
+            }
+            for(int j=0;j<(2*i+1);j++){
+                System.out.printf("*");
+            }
+             for(int j=0;j<space;j++){
+                System.out.printf(" ");
+        }
+        System.out.println();
+            }
+        }
+    }
